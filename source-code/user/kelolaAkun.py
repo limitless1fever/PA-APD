ac7e15d119e6de4d2077639d837f2c88975263ee
Altairstry <pirlosyabilahafuza@gmail.com>
2025-11-20 22:15:10 +0800

    feat: tambah menu kelola akun

--- source-code/user/kelolaAkun.py
+++ source-code/user/kelolaAkun.py
@@ -1,7 +1,34 @@
 # Pengelolaan Akun & data Pribadi
+from fungsi.utilitas import clear
 
-# Fungsi Demonstrasi, Bisa diganti sesuai kemauan
-def kelolaAkun(): 
-    print("=" * 75)
-    print("Kelola Akun")
-    print("=" * 75)
+# Fungsi Fungsi Masih Kosong
+def ubah_data_akun():
+    pass
+
+def ubah_data_pribadi():
+    pass
+
+# Menu Kelola Akun
+def kelolaAkun():
+    while True:
+        clear
+        print("=" * 75)
+        print("KELOLA AKUN")
+        print("=" * 75)
+        print("[1] - Ubah Data pada Akun")
+        print("[2] -  Ubah Data Pribadi")
+        print("[0] - Kembali ke Menu Sebelumnya")
+        print("=" * 75)
+        pilih = input("> ")
+
+        if pilih == "0":
+            break
+
+        elif pilih == "1":
+            ubah_data_akun()
+        
+        elif pilih == "2":
+            ubah_data_pribadi()
+        
+        else:
+            print("Pilihan tidak valid")
